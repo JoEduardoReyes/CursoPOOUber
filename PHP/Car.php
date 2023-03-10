c529f8ce95fe37e39de27639bb6140a306d3f362
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-03-10 10:35:06 -0600

    Creamos las clases en PHP

--- PHP/Car.php
+++ PHP/Car.php
@@ -1,12 +1,14 @@
 <?php
 
+require_once('Account.php');
+
 class Car {
   private $id;
   private $license;
   private $driver;
   private $passenger;
 
-  public function __construct($id, $license, $driver, $passenger) {
+  public function __construct($id, $license, Account $driver, $passenger) {
     $this->id = $id;
     $this->license = $license;
     $this->driver = $driver;
--- PHP/main.php
+++ PHP/main.php
@@ -1,3 +1,8 @@
 <?php
 
-print_r("Hola mundo");
+require_once('Account.php');
+require_once('Car.php');
+
+$car = new Car(1, "QW456", new Account(1,"John Doe", "QW456", "john@example.com", "1234"), 2);
+
+var_dump($car);
